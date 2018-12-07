fe06ac96b9d60886d19aca8448c2528e7c25fb31
takata7 <takata@robomind.co.jp>
2018-12-07 10:22:17 +0900

    initial2

---
+++ VS2017Cooperation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS2017Cooperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+        }
+    }
+}
